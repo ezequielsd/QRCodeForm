7bdfc17a87ca657f4d272cb1200fd898375c6d9d
Ezequiel.Daniel <ezequiel.daniel@selbetti.com.br>
2018-05-04 16:19:08 -0300

    Versionando o projeto

---
+++ QRCodeForm/frmMain.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace QRCodeForm
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTextoQRCode = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtNomeQRCode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAbrirLocalizacao = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGerar.Location = new System.Drawing.Point(480, 157);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(119, 23);
+            this.btnGerar.TabIndex = 3;
+            this.btnGerar.Text = "Gerar QRCode";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Inserir o texto a ser gravado no QRCode:";
+            // 
+            // txtTextoQRCode
+            // 
+            this.txtTextoQRCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTextoQRCode.Location = new System.Drawing.Point(14, 131);
+            this.txtTextoQRCode.Name = "txtTextoQRCode";
+            this.txtTextoQRCode.Size = new System.Drawing.Size(585, 22);
+            this.txtTextoQRCode.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(168, 167);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtNomeQRCode
+            // 
+            this.txtNomeQRCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNomeQRCode.Location = new System.Drawing.Point(14, 76);
+            this.txtNomeQRCode.Name = "txtNomeQRCode";
+            this.txtNomeQRCode.Size = new System.Drawing.Size(304, 22);
+            this.txtNomeQRCode.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(271, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Insera um nome para o arquivo de QRCode:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(225, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Gerador de QRCode";
+            // 
+            // btnAbrirLocalizacao
+            // 
+            this.btnAbrirLocalizacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbrirLocalizacao.Location = new System.Drawing.Point(480, 190);
+            this.btnAbrirLocalizacao.Name = "btnAbrirLocalizacao";
+            this.btnAbrirLocalizacao.Size = new System.Drawing.Size(119, 23);
+            this.btnAbrirLocalizacao.TabIndex = 7;
+            this.btnAbrirLocalizacao.Text = "Abrir localização";
+            this.btnAbrirLocalizacao.UseVisualStyleBackColor = true;
+            this.btnAbrirLocalizacao.Click += new System.EventHandler(this.btnAbrirLocalizacao_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(25, 194);
+            this.trackBar1.Maximum = 2;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 104);
+            this.trackBar1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(56, 277);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "150 x 150";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(56, 240);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "200 x 200";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(56, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "250 x 250";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(16, 165);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(141, 16);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Tamanho do QRCode";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 397);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.btnAbrirLocalizacao);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNomeQRCode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtTextoQRCode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGerar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QRCode Form";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTextoQRCode;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtNomeQRCode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAbrirLocalizacao;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ QRCodeForm/frmMain.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+namespace QRCodeForm
+{
+    public partial class frmMain : Form
+    {
+        private string filePath = Path.Combine(Environment.CurrentDirectory, "QRCode");
+        private string fileLocalitation = String.Empty;
+        private int width = 150;
+        private int height = 150;
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            if (!Directory.Exists(filePath))
+                Directory.CreateDirectory(filePath);
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(txtTextoQRCode.Text) && !String.IsNullOrWhiteSpace(txtTextoQRCode.Text))
+                {
+                    switch (trackBar1.Value)
+                    {
+                        case 0:
+                            width = 150;
+                            height = 150;
+                            break;
+                        case 1:
+                            width = 200;
+                            height = 200;
+                            break;
+                        case 2:
+                            width = 250;
+                            height = 250;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    var barcodeWriter = new BarcodeWriter();
+                    barcodeWriter.Format = BarcodeFormat.QR_CODE;
+                    barcodeWriter.Options = new ZXing.Common.EncodingOptions
+                    {
+                        Width = width,
+                        Height = height
+                    };
+
+                    fileLocalitation = String.Format(@"{0}\{1}.jpg", filePath, txtNomeQRCode.Text);
+
+                    barcodeWriter.Write(txtTextoQRCode.Text).Save(fileLocalitation, ImageFormat.Jpeg);
+                                        
+                    if (File.Exists(fileLocalitation))
+                    {
+                        pictureBox1.Size = new Size(width, height);
+                        pictureBox1.ImageLocation = fileLocalitation;
+                    }
+                }
+                else
+                    MessageBox.Show("Informe um texto a ser gravado no QRCode", "Informe um texto!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocorreu um erro ao gerar QRCode {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);                
+            }
+            finally
+            {
+                txtNomeQRCode.Clear();
+                txtTextoQRCode.Clear();
+            }
+        }
+
+        private void btnAbrirLocalizacao_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(filePath))
+            {
+                Process.Start(filePath);
+            }
+        }
+    }
+}
